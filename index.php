6ad478b9ffe3c457c36268fafc1f28566f959f22
Togrul Salmanli <togrulsalmanli33@gmail.com>
2021-03-01 12:25:20 +0400

    php

--- index.php
+++ index.php
@@ -4,12 +4,26 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="./Style/style.css"/>
-	<title>PHP PROJECT</title>
+	<link rel="stylesheet" href="./css/style.css"/>
+
+	<title>PHP Crud</title>
 </head>
 <body>
-<?php $ad="Togrul";$soyad="Salmanli"?>
-	<h1><?=$ad?></h1>
-	<h2><?=$soyad?></h2>
+
+	<div class="container">
+
+		<h1>PHP CRUD</h1>
+
+		<form action="" method="POST"> 
+			<input type="text" placeholder="First Name" name="firstname" value="" required/>
+			<input type="text" placeholder="Last Name" name="lastname" value="" required/>
+			<input type="email" placeholder="Enter Email" name="email" value="" required/>
+			<input type="tel" placeholder="Enter Phone Number" name="phone" value="" required/>
+			<textarea name="adres" placeholder="Enter Address" rows="8" cols="20"></textarea>
+			<button class="button" type="submit">Add Crud</button>
+		</form>
+
+	</div>
+
 </body>
 </html>
